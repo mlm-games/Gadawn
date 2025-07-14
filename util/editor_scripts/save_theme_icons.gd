20f152ad17f36ab420933c216479ea1325bed48c
mlm-games <125530737+mlm-games@users.noreply.github.com>
2025-07-14 13:35:49 +0530

    fix theme path

--- util/editor_scripts/save_theme_icons.gd
+++ util/editor_scripts/save_theme_icons.gd
@@ -2,10 +2,10 @@
 extends EditorScript
 
 func _run():
-	var theme: Theme = load("res://Themes/default/GoDawnTheme.tres")
+	var theme: Theme = load("res://themes/default/go_dawn_theme.tres")
 	var node_type = "VSplitContainer"
 
 	var icons = theme.get_icon_list(node_type)
 	for icon in icons:
-		var tex: ImageTexture = theme.get_icon(icon, node_type)
-		tex.get_data().save_png("res://Themes/default/ButtonIcons/%s.png" % icon)
+		var tex: Image = theme.get_icon(icon, node_type).get_image()
+		tex.save_png("res://themes/default/button_icons/%s.png" % icon)
